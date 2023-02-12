955732f5519fcb08aaccfd7dc2693f05fc673e88
FranciscoManuel96 <72860691+FranciscoManuel96@users.noreply.github.com>
2023-02-12 23:00:45 +0000

    Word Boards, Levels Design and some visuals

--- Assets/Scripts/LettersForDrag.cs
+++ Assets/Scripts/LettersForDrag.cs
@@ -11,34 +11,64 @@ public class LettersForDrag : MonoBehaviour
     [SerializeField] Transform centerDraggingArea;
     [SerializeField] string[] levelLetters;
     [SerializeField] GameObject textBox;
+    [SerializeField] float radiusLetter = 0.35f;
+    Camera mainCamera;
 
-    int temp = 3;
+    int level = 1;
     int numbLetters;
 
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        LettersForLevel(temp);
-        print(numbLetters);
+        mainCamera = Camera.main;
+        LettersForLevel(level);
+        GenerateLetters();
+    }
+
+    //[ContextMenu("generate")]
+    private void GenerateLetters()
+    {
         for (int i = 0; i < numbLetters; i++)
         {
-            //circunferencia de raio radius com letras espalhadas pelo Pi de acordo com a quantidade de letras
-            Vector3 pos = transform.position + new Vector3(Mathf.Cos(i*(2 * Mathf.PI) / numbLetters), Mathf.Sin(i * (2 * Mathf.PI) / numbLetters), -1) * radius;
+            //print the letters of the level on the dragging area through a circumference depending on the number of letters the level has
+            Vector3 pos = transform.position + new Vector3(Mathf.Cos(i * (2 * Mathf.PI) / numbLetters), Mathf.Sin(i * (2 * Mathf.PI) / numbLetters), -1) * radius;
             GameObject createdLetter = Instantiate(textBox, pos, Quaternion.identity, transform);
             createdLetter.GetComponent<TextMeshProUGUI>().text = levelLetters[i];
-            print("teste");
+        }
+    }
+
+    void Update()
+    {
+        //detetar touch (posiçao inicial e se interceptou algum dos colliders das letras)
+        //if (Input.touchCount > 0)
+        //{
+        //    print("touch");
+        //    Vector2 touchPoint = Input.touches[0].position;
+        //    if (Vector2.Distance(touchPoint, (Vector2)transform.position + Vector2.right) <= radiusLetter)
+        //    {
+        //        Debug.Log("acertou");
+        //    }
+        //}
+        if (Input.GetMouseButton(0))
+        {
+            
+            Vector2 touchPoint = Input.mousePosition - transform.position;
+            print(transform.position);
+            //print(touchPoint);
+            //print((Vector2)transform.position + Vector2.right);
+            if (Vector2.Distance(touchPoint, (Vector2)transform.position + Vector2.right) <= radiusLetter)
+            {
+                Debug.Log("acertou");
+            }
         }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
+    //guardar a letra do respectivo collider onde tocou numa variavel
+    //verificar se a palavra formada esta na grid (+2 pontos) ou se está na wordlist (+1 ponto)
+    //
+
+    //Hardcoded letters for the levels
     public void LettersForLevel(int level)
     {
         if (level == 1) 
@@ -62,5 +92,15 @@ public class LettersForDrag : MonoBehaviour
             levelLetters = new string[] { "R", "A", "D", "T", "I", "A", "E", "N" };
         }
         numbLetters = levelLetters.Length;
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < numbLetters; i++)
+        {
+            //print the letters of the level on the dragging area through a circumference depending on the number of letters the level has
+            Vector3 pos = transform.position + new Vector3(Mathf.Cos(i * (2 * Mathf.PI) / numbLetters), Mathf.Sin(i * (2 * Mathf.PI) / numbLetters), -1) * radius;
+            Gizmos.DrawSphere(pos, radiusLetter);
+        }
     }
 }
--- Assets/Scripts/WordListReader.cs
+++ Assets/Scripts/WordListReader.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using TMPro;
 using UnityEngine;
 
 public class WordListReader : MonoBehaviour
 {
+    public HashSet<string> words;
+    [SerializeField] TextAsset csvFile;
 
     //usar hashset para guardar a lista de palavras
+    void ReadCSV()
+    {
+        string[] tempWords = csvFile.text.Split("\r\n");
+        words = new HashSet<string>(tempWords);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        ReadCSV();
     }
 }
